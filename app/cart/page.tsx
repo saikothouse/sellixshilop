48c773fa4f7cc8ec2c748314b362989f958bd712
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-08 00:48:41 +0600

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from "next/image";
 import { FaTrash, FaArrowRight } from 'react-icons/fa';
 
 interface Product {
