a421066a90349a038148e7b6abaccc4538ebae51
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-08 00:44:21 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Image from "next/image";
 import { FaInfoCircle, FaShoppingBag } from 'react-icons/fa';
+import Image from "next/image";
 
 interface Product {
   id: string;
